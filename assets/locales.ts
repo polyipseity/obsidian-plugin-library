055a494ebf5466ef55a6e5ccc2e4bac5daa357ef
William So <polyipseity@gmail.com>
2023-05-13 10:04:53 +0800

    Rename a function
    Signed-off-by: William So <polyipseity@gmail.com>

--- assets/locales.ts
+++ assets/locales.ts
@@ -101,212 +101,212 @@ export namespace LibraryLocales {
 			capitalize: lng => (value): string => capitalize(String(value), lng),
 			uncapitalize: lng => (value): string => uncapitalize(String(value), lng),
 		}) satisfies I18nFormatters
-	const syncLocale0 = syncLocale<typeof en>()
+	const sync = syncLocale<typeof en>()
 	// Sync with https://github.com/obsidianmd/obsidian-translations#existing-languages (e266f1f2171102e8b8c607fec7c8b494e22b25b4)
 	export const RESOURCES = deepFreeze({
 		af: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/af/translation.json")).default),
+				sync((await import("./locales/af/translation.json")).default),
 		},
 		am: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/am/translation.json")).default),
+				sync((await import("./locales/am/translation.json")).default),
 		},
 		ar: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ar/translation.json")).default),
+				sync((await import("./locales/ar/translation.json")).default),
 		},
 		be: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/be/translation.json")).default),
+				sync((await import("./locales/be/translation.json")).default),
 		},
 		bg: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/bg/translation.json")).default),
+				sync((await import("./locales/bg/translation.json")).default),
 		},
 		bn: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/bn/translation.json")).default),
+				sync((await import("./locales/bn/translation.json")).default),
 		},
 		ca: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ca/translation.json")).default),
+				sync((await import("./locales/ca/translation.json")).default),
 		},
 		cs: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/cs/translation.json")).default),
+				sync((await import("./locales/cs/translation.json")).default),
 		},
 		da: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/da/translation.json")).default),
+				sync((await import("./locales/da/translation.json")).default),
 		},
 		de: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/de/translation.json")).default),
+				sync((await import("./locales/de/translation.json")).default),
 		},
 		el: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/el/translation.json")).default),
+				sync((await import("./locales/el/translation.json")).default),
 		},
 		en: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/en/translation.json")).default),
+				sync((await import("./locales/en/translation.json")).default),
 			asset: async () => (await import("./locales/en/asset.json")).default,
 			language: async () =>
 				(await import("./locales/en/language.json")).default,
 		},
 		eo: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/eo/translation.json")).default),
+				sync((await import("./locales/eo/translation.json")).default),
 		},
 		es: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/es/translation.json")).default),
+				sync((await import("./locales/es/translation.json")).default),
 		},
 		eu: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/eu/translation.json")).default),
+				sync((await import("./locales/eu/translation.json")).default),
 		},
 		fa: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/fa/translation.json")).default),
+				sync((await import("./locales/fa/translation.json")).default),
 		},
 		fi: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/fi/translation.json")).default),
+				sync((await import("./locales/fi/translation.json")).default),
 		},
 		fr: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/fr/translation.json")).default),
+				sync((await import("./locales/fr/translation.json")).default),
 		},
 		gl: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/gl/translation.json")).default),
+				sync((await import("./locales/gl/translation.json")).default),
 		},
 		he: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/he/translation.json")).default),
+				sync((await import("./locales/he/translation.json")).default),
 		},
 		hi: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/hi/translation.json")).default),
+				sync((await import("./locales/hi/translation.json")).default),
 		},
 		hu: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/hu/translation.json")).default),
+				sync((await import("./locales/hu/translation.json")).default),
 		},
 		id: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/id/translation.json")).default),
+				sync((await import("./locales/id/translation.json")).default),
 		},
 		it: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/it/translation.json")).default),
+				sync((await import("./locales/it/translation.json")).default),
 		},
 		ja: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ja/translation.json")).default),
+				sync((await import("./locales/ja/translation.json")).default),
 		},
 		ko: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ko/translation.json")).default),
+				sync((await import("./locales/ko/translation.json")).default),
 		},
 		lv: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/lv/translation.json")).default),
+				sync((await import("./locales/lv/translation.json")).default),
 		},
 		ml: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ml/translation.json")).default),
+				sync((await import("./locales/ml/translation.json")).default),
 		},
 		ms: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ms/translation.json")).default),
+				sync((await import("./locales/ms/translation.json")).default),
 		},
 		nl: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/nl/translation.json")).default),
+				sync((await import("./locales/nl/translation.json")).default),
 		},
 		no: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/no/translation.json")).default),
+				sync((await import("./locales/no/translation.json")).default),
 		},
 		oc: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/oc/translation.json")).default),
+				sync((await import("./locales/oc/translation.json")).default),
 		},
 		pl: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/pl/translation.json")).default),
+				sync((await import("./locales/pl/translation.json")).default),
 		},
 		pt: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/pt/translation.json")).default),
+				sync((await import("./locales/pt/translation.json")).default),
 		},
 		// eslint-disable-next-line @typescript-eslint/naming-convention
 		"pt-BR": {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/pt-BR/translation.json")).default),
+				sync((await import("./locales/pt-BR/translation.json")).default),
 		},
 		ro: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ro/translation.json")).default),
+				sync((await import("./locales/ro/translation.json")).default),
 		},
 		ru: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ru/translation.json")).default),
+				sync((await import("./locales/ru/translation.json")).default),
 		},
 		se: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/se/translation.json")).default),
+				sync((await import("./locales/se/translation.json")).default),
 		},
 		sk: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/sk/translation.json")).default),
+				sync((await import("./locales/sk/translation.json")).default),
 		},
 		sq: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/sq/translation.json")).default),
+				sync((await import("./locales/sq/translation.json")).default),
 		},
 		sr: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/sr/translation.json")).default),
+				sync((await import("./locales/sr/translation.json")).default),
 		},
 		ta: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ta/translation.json")).default),
+				sync((await import("./locales/ta/translation.json")).default),
 		},
 		te: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/te/translation.json")).default),
+				sync((await import("./locales/te/translation.json")).default),
 		},
 		th: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/th/translation.json")).default),
+				sync((await import("./locales/th/translation.json")).default),
 		},
 		tr: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/tr/translation.json")).default),
+				sync((await import("./locales/tr/translation.json")).default),
 		},
 		uk: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/uk/translation.json")).default),
+				sync((await import("./locales/uk/translation.json")).default),
 		},
 		ur: {
 			[DEFAULT_NAMESPACE]: async () =>
-				syncLocale0((await import("./locales/ur/translation.json")).default),
+				sync((await import("./locales/ur/translation.json")).default),
 		},
 		// eslint-disable-next-line @typescript-eslint/naming-convention
 		"zh-Hans": {
-			[DEFAULT_NAMESPACE]: async () => syncLocale0(
-				(await import("./locales/zh-Hans/translation.json")).default,
-			),
+			[DEFAULT_NAMESPACE]: async () =>
+				sync((await import("./locales/zh-Hans/translation.json")).default,
+				),
 		},
 		// eslint-disable-next-line @typescript-eslint/naming-convention
 		"zh-Hant": {
-			[DEFAULT_NAMESPACE]: async () => syncLocale0(
-				(await import("./locales/zh-Hant/translation.json")).default,
-			),
+			[DEFAULT_NAMESPACE]: async () =>
+				sync((await import("./locales/zh-Hant/translation.json")).default,
+				),
 		},
 	}) satisfies I18nResources
 	export type Resources =
