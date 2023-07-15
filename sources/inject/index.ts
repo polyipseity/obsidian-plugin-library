7369e16810e55ee6aa763197602fa48b2ea60158
William So <polyipseity@gmail.com>
2023-07-15 19:50:49 +0800

    Create export `@polyipseity/obsidian-plugin-library/inject`
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ sources/inject/index.ts
@@ -0,0 +1,19 @@
+import type { } from "obsidian"
+
+export function requestAnimationFrame(
+	...args: Parameters<AnimationFrameProvider["requestAnimationFrame"]>
+): ReturnType<AnimationFrameProvider["requestAnimationFrame"]> {
+	return self.activeWindow.requestAnimationFrame(...args)
+}
+
+export function setInterval(
+	...args: Parameters<WindowOrWorkerGlobalScope["setInterval"]>
+): ReturnType<WindowOrWorkerGlobalScope["setInterval"]> {
+	return self.activeWindow.setInterval(...args)
+}
+
+export function setTimeout(
+	...args: Parameters<WindowOrWorkerGlobalScope["setTimeout"]>
+): ReturnType<WindowOrWorkerGlobalScope["setTimeout"]> {
+	return self.activeWindow.setTimeout(...args)
+}
