efc2ffc16a36803759419fc4aea4774d16f3344a
William So <polyipseity@gmail.com>
2024-06-06 14:30:27 +0800

    Fix misdetecting iPadOS as Mac
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ .changeset/tricky-phones-tickle.md
@@ -0,0 +1,5 @@
+---
+"@polyipseity/obsidian-plugin-library": patch
+---
+
+Fix misdetecting iPadOS as Mac.
--- sources/platform.ts
+++ sources/platform.ts
@@ -1,5 +1,6 @@
 import type { LibraryUUIDs } from "./magic.js"
 import type { Opaque } from "ts-essentials"
+import { Platform as Platform0 } from "obsidian"
 import { deepFreeze } from "./util.js"
 
 export namespace Platform {
@@ -12,21 +13,23 @@ export namespace Platform {
 	export type All = typeof ALL[number]
 	export type Current = Opaque<All, typeof LibraryUUIDs["UUID2"]>
 	export const CURRENT = ((): All => {
-		const { userAgent } = self.navigator
-		if (userAgent.includes("like Mac")) {
+		if (Platform0.isIosApp) {
 			return "ios"
 		}
-		if (userAgent.includes("Android")) {
+		if (Platform0.isAndroidApp) {
 			return "android"
 		}
-		if (userAgent.includes("Mac")) {
-			return "darwin"
-		}
-		if (userAgent.includes("Win")) {
-			return "win32"
-		}
-		if (userAgent.includes("Linux") || userAgent.includes("X11")) {
-			return "linux"
+		if (Platform0.isDesktopApp) {
+			const { userAgent } = self.navigator
+			if (userAgent.includes("Mac")) {
+				return "darwin"
+			}
+			if (userAgent.includes("Win")) {
+				return "win32"
+			}
+			if (userAgent.includes("Linux") || userAgent.includes("X11")) {
+				return "linux"
+			}
 		}
 		return "unknown"
 	})() as Current
