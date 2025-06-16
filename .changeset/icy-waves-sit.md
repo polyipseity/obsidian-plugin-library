d22bbc463963297709fa5a24729f8c5a9eb80252
William So <polyipseity@gmail.com>
2025-06-16 13:07:44 +0800

    Expand private typings in `obsidian.ts`

---
+++ .changeset/icy-waves-sit.md
@@ -0,0 +1,5 @@
+---
+"@polyipseity/obsidian-plugin-library": minor
+---
+
+Expand private typings in `obsidian.ts`.
--- src/@types/obsidian.ts
+++ src/@types/obsidian.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 declare module "obsidian" {
 	interface App extends Private<$App, PrivateKey> { }
+	interface BakedHotkey extends Private<$BakedHotkey, PrivateKey> { }
+	interface Commands extends Private<$Commands, PrivateKey> { }
 	interface CommunityPluginsSettingTab
 		extends Private<$CommunityPluginsSettingTab, PrivateKey> { }
 	interface DataAdapter extends Private<$DataAdapter, PrivateKey> { }
 	interface FileSystem extends Private<$FileSystem, PrivateKey> { }
+	interface HotkeyManager extends Private<$HotkeyManager, PrivateKey> { }
+	interface Keymap extends Private<$Keymap, PrivateKey> { }
 	interface Plugins extends Private<$Plugins, PrivateKey> { }
 	namespace Plugins {
 		type Map<I extends string> = I extends keyof Mapping ? Mapping[I] : Plugin
@@ -13,7 +17,7 @@ declare module "obsidian" {
 	interface Scope {
 		// eslint-disable-next-line @typescript-eslint/method-signature-style
 		register(
-			modifiers: readonly Modifier[],
+			modifiers: readonly Modifier[] | null,
 			key: string | null,
 			func: KeymapEventListener,
 		): KeymapEventHandler
@@ -26,14 +30,22 @@ declare module "obsidian" {
 	interface WorkspaceRibbon extends Private<$WorkspaceRibbon, PrivateKey> { }
 }
 import type {
+	BakedHotkey,
+	Command,
+	Commands,
 	CommunityPluginsSettingTab,
 	FileSystem,
+	Hotkey,
+	HotkeyManager,
+	Keymap,
+	KeymapContext,
 	PluginManifest,
 	Plugins,
 	SettingTab,
 	UnknownSettingTab,
+	UserEvent,
 } from "obsidian"
-import type { Private } from "../private.js"
+import type { Private, RevealPrivate } from "../private.js"
 
 declare const PRIVATE_KEY: unique symbol
 type PrivateKey = typeof PRIVATE_KEY
@@ -45,11 +57,20 @@ declare module "../private.js" {
 
 interface $App {
 	readonly appId: string
+	readonly commands: Commands
+	readonly hotkeyManager: HotkeyManager
 	readonly plugins: Plugins
 	readonly setting: {
 		readonly settingTabs:
 		readonly (CommunityPluginsSettingTab | UnknownSettingTab)[]
 	}
+}
+
+interface $BakedHotkey { }
+
+interface $Commands {
+	readonly executeCommand: (command: Command, event?: UserEvent) => boolean
+	readonly findCommand: (id?: string) => Command | undefined
 }
 
 interface $CommunityPluginsSettingTab extends SettingTab {
@@ -68,6 +89,23 @@ interface $FileSystem {
 	readonly open?: <Length extends number>(
 		path: Length extends 1 ? string : never,
 	) => Length extends 1 ? PromiseLike<void> : never
+}
+
+interface $HotkeyManager {
+	readonly bake: () => void
+	baked: boolean
+	bakedHotkeys: BakedHotkey[]
+	bakedIds: string[]
+	readonly customKeys: Record<string, Hotkey[]>
+	defaultKeys: Record<string, Hotkey[]>
+	readonly removeHotkeys: (id: string) => void
+	readonly setHotkeys: (id: string, hotkey: readonly Hotkey[]) => void
+}
+
+interface $Keymap {
+	readonly constructor: typeof Keymap & {
+		readonly isMatch: (key: RevealPrivate<BakedHotkey>, ctx: KeymapContext) => boolean
+	}
 }
 
 interface $Plugins {
